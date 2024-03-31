7d3dfd6e61cfc307a87916baba8f1896ceddf4e5
iljuhenson <ilyapavluk24@gmail.com>
2024-03-31 16:06:05 +0200

    Add themes

---
+++ frontend/src/components/Card/Card.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// interface CardProps {
+//     headerComponent: React.FC,
+//     mainComponent: React.FC,
+// }
+
+function Card(/*{ headerComponent, mainComponent }: CardProps*/) {
+    return (
+        <></>
+    );
+}
+
+export default Card;
---
+++ frontend/src/components/Themes/default.ts
@@ -0,0 +1,46 @@
+export default {
+    colors: {
+        app: {
+            background: "#F5F5F7",
+            formBackground: "#F5F5F7",
+        },
+        tasks: {
+            easy: "#7FC8F8",
+            medium: "#FFDF38",
+            hard: "#FF5A5F",
+            completed: "#DCDCDC",
+        },
+
+        shop: {
+            available: "#7FC8F8",
+            purchased: "#DCDCDC",
+
+        },
+
+        calendar: {
+            active: "#FFDF38",
+        },
+
+        card: {
+            background: "#FFFFFF",
+            header: "#DCDCDC",
+        },
+
+        text: {
+            primary: "#242834",
+            secondary: "#585259",
+        }
+    },
+    sizes: {
+        desktop: {
+            text: {
+                title: 20,
+                content: 16,
+            },
+            icon: 42,
+
+        },
+        mobile: {}
+    }
+
+}
