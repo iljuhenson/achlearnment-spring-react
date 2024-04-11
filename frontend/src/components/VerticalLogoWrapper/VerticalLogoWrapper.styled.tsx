be2c3197c089bbf25894e6baf59dd8a3890ff303
iljuhenson <ilyapavluk24@gmail.com>
2024-04-11 14:19:37 +0200

    Rotate achlearnment logo on Tasks page

--- frontend/src/components/VerticalLogoWrapper/VerticalLogoWrapper.styled.tsx
+++ frontend/src/components/VerticalLogoWrapper/VerticalLogoWrapper.styled.tsx
@@ -8,17 +8,22 @@ export default styled.div`
     border-radius: 16px;
     overflow: hidden;
     box-shadow: 0 12px 40px 4px rgba(112, 144, 176, 0.2);
-    font-size: ${({ theme }) => theme.sizes.desktop.text.title }px;
     font-weight: bold;
-    
+    display: flex;
+    justify-content: center;
+    align-items: center;
     p {
-        position: absolute;
+        //position: absolute;
+        font-size: ${({ theme }) => theme.sizes.desktop.text.title }px;
+        writing-mode: tb-rl;
+            //transform: rotate(-180deg);
         
-        transform: rotate(90deg) translate(0%, 100%); 
-        top: 50%;
+        //transform: translate(-50%, -50%); 
+        //transform-origin: bottom left;
+        //top: 50%;
         //left: 50%;
         //top: 0;
-        left: 0;
+        //right: -100%;
         //transform: ;
         
         
